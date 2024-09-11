ec4d1f8df0b56416c80a781f8687a8076bb7a33b
Mrugesh Dixit <mrugesh.dixit@irisbusiness.com>
2024-09-11 18:20:57 +0530

    Add validations

--- src/dashboard/create-task/create-task.component.ts
+++ src/dashboard/create-task/create-task.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe, formatDate } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -7,6 +7,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { ITask } from '../../Models/global';
+import { FormErrorService } from '../../service/FormError/form-error.service';
 @Component({
   selector: 'app-create-task',
   standalone: true,
@@ -16,15 +17,25 @@ import { ITask } from '../../Models/global';
 })
 export class CreateTaskComponent {
   @Output() onCloseBtnClick: EventEmitter<ITask> = new EventEmitter<ITask>();
+  errorService: FormErrorService = inject(FormErrorService);
 
   taskForm: FormGroup = new FormGroup({
-    title: new FormControl('', Validators.required),
+    title: new FormControl('', [Validators.required, Validators.maxLength(20)]),
     description: new FormControl('', Validators.required),
     assignedTo: new FormControl('', Validators.required),
     createdAt: new FormControl(formatDate(new Date(), 'yyyy-MM-dd', 'en')),
-    priority: new FormControl('Low', Validators.required),
-    status: new FormControl('Open', Validators.required),
+    priority: new FormControl('Low'),
+    status: new FormControl('Open'),
   });
+  shouldShowError(controlName: string): boolean {
+    const control = this.taskForm.get(controlName) as FormControl;
+    return this.errorService.shouldShowError(control);
+  }
+
+  getError(controlName: string, controlLabel?: string): string {
+    const control = this.taskForm.get(controlName) as FormControl;
+    return this.errorService.getError(control, controlLabel);
+  }
   handleSubmit() {
     this.onCloseBtnClick.emit(this.taskForm.value);
   }
